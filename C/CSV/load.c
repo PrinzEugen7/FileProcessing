4d61ee69aa71779d4811958585d1584d6cd2903e
PrinzEugen7 <nsi05162@pooae.com>
2016-08-28 00:53:34 +0900

    Create load.c

---
+++ C/CSV/load.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(void)
+{
+	FILE *fp;
+	char *fname = "test.csv";
+	char data1[100];
+	float data2, data3;
+	int ret;
+
+	fp = fopen( fname, "r" );
+	if( fp == NULL ){
+		printf( "Not open %s¥n", fname );
+		return -1;
+	}
+
+	while( ( ret = fscanf( fp, "%[^,],%f,%f", s, &data1, &data2) ) != EOF ){
+		printf( "%s %f %f", data1, data2, data3 );
+	}
+
+	fclose( fp );
+	return 0;
+}
