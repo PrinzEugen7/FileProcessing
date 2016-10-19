1c30b2b8292cd3eb467d15167fd5c7f47b95b626
PrinzEugen7 <nsi05162@pooae.com>
2016-10-19 22:13:27 +0900

    Create compression.py

---
+++ Python/Basic/Zip/compression.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+import zipfile
+
+def main():
+	zip = zipfile.ZipFile("abc.zip", "w", zipfile.ZIP_DEFLATED)
+	zip.write("a.jpg')
+	zip.write("b.png')
+	zip.write("c.txt')
+	zip.close()
+
+if __name__ == "__main__":
+    main()
