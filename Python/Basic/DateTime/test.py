dfb4f00d4145ee7aa80604093bb1e06c97489f58
PrinzEugen7 <nsi05162@pooae.com>
2016-12-12 19:33:58 +0900

    Create test.py

---
+++ Python/Basic/DateTime/test.py
@@ -0,0 +1,10 @@
+import datetime
+    
+def main():
+    today        =    datetime.date.today()
+    todaydetail  =    datetime.datetime.today()
+    print(today)
+    print(todaydetail)
+
+if __name__ == "__main__":
+    main()
