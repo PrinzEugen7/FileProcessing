78fe4f642da2f95770fc602c75bd10b3bef2d279
PrinzEugen7 <nsi05162@pooae.com>
2017-01-16 12:04:36 +0900

    Update compression.py

--- Python/Basic/Zip/compression.py
+++ Python/Basic/Zip/compression.py
@@ -3,9 +3,9 @@ import zipfile
 
 def main():
 	zip = zipfile.ZipFile("abc.zip", "w", zipfile.ZIP_DEFLATED)
-	zip.write("a.jpg')
-	zip.write("b.png')
-	zip.write("c.txt')
+	zip.write("a.jpg")
+	zip.write("b.png")
+	zip.write("c.txt")
 	zip.close()
 
 if __name__ == "__main__":
