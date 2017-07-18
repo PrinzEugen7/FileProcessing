439107327007300f5da9a1c7a5596d2db895f756
PrinzEugen <nsi05162@pooae.com>
2017-07-18 12:00:39 +0900

    Update hms.py

--- Python/Basic/DateTime/hms.py
+++ Python/Basic/DateTime/hms.py
@@ -5,7 +5,7 @@ def main():
     today = datetime.datetime.today()
     print(today)
     print(today.year, "年", today.month,"月",today.day, "日")
-    print(today.hour, "時", today.minute, "分", today.second, "秒", today.microsecond, "ミリ秒")
+    print(today.hour, "時", today.minute, "分", today.second, "秒", today.microsecond, "マイクロ秒")
 
 if __name__ == "__main__":
     main()
